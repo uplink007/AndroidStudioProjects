c4d96fe243803d5828f89c33f118f0ee904daeff
sergesh <sergesh@ac.sce.ac.il>
2017-12-03 10:40:48 +0200

    Initial commit
    sad

---
+++ HW_1/app/src/main/java/com/example/sergeyshevchuck/hw_1/HelloAndroid.java
@@ -0,0 +1,14 @@
+package com.example.sergeyshevchuck.hw_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelloAndroid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_android);
+    }
+}
